6ec281d0601e5951c0fe20f0e154fce689856490
Zeeshan Khan <zeeshan.saeed8323@gmail.com>
2025-12-06 20:16:28 +0500

    Update database.js

--- 03-API-&-MangoDB/database/database.js
+++ 03-API-&-MangoDB/database/database.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://username:password@cluster0.nyex9ev.mongodb.net/techno?appName=Cluster0`)
+        await mongoose.connect(`mongodb+srv://username:password@cluster0.nyex9ev.mongodb.net/product?appName=Cluster0`)
         console.log(`Connected to MongoDB!`);
     } catch (error) {
-        console.log("OH NO ERROR");
+        console.log("OH ERROR");
         console.log(error);
     }
 }
