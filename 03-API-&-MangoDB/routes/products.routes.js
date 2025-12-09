d8e13480b1557c99ba877f4cc50818e6c3142602
Zeeshan Khan <zeeshan.saeed8323@gmail.com>
2025-12-09 21:09:10 +0500

    Remove unnecessary empty line in products.routes.js

--- 03-API-&-MangoDB/routes/products.routes.js
+++ 03-API-&-MangoDB/routes/products.routes.js
@@ -1,6 +1,5 @@
 import express from "express"
 const router = express.Router()
-
 import { createProduct, readAllProducts } from "../controllers/products.controllers.js"
 
 // create product "/products"  
